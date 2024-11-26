627c1d2bc56d9ce8f21e48aace445e70b172e5e2
Waqas Ahmed <waqasahmed.uol@gmail.com>
2024-11-26 22:16:11 +0500

    Initial Commit

---
+++ Sources/AppImage/BannerImage.swift
@@ -0,0 +1,27 @@
+//
+//  BannerImage.swift
+//  AppImage
+//
+//  Created by waqas ahmed on 26/11/2024.
+//
+import Foundation
+import SwiftUI
+
+public struct BannerImage{
+    var image: String
+    var height: CGFloat = 200
+    var width: CGFloat = 0
+    var paddingTop: CGFloat = 10
+    var cornerRadius: CGFloat = 25
+    var padding: CGFloat = 0
+    public var body: some View {
+        Image(image)
+            .resizable()
+            //.scaledToFit() // Ensures the image fills the specified frame
+            .frame(width: width, height: height) // Specify only the height; width will adjust automatically
+            .cornerRadius(cornerRadius) // Apply corner radius
+            .padding(.horizontal, padding) // Apply horizontal padding to the image container
+            .padding(.top, paddingTop)
+            .clipped() // Clips any overflowing content to fit within the corner radius
+    }
+}
