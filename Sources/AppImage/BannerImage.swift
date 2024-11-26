e6f44cafde6ae6977dcbc1a4f58c1f1e0fd5fa73
Waqas Ahmed <waqasahmed.uol@gmail.com>
2024-11-26 23:39:04 +0500

    update code

--- Sources/AppImage/BannerImage.swift
+++ Sources/AppImage/BannerImage.swift
@@ -7,22 +7,31 @@
 import Foundation
 import SwiftUI
 
-public struct BannerImage{
+public struct BannerImage {
     public var image: String
     public var height: CGFloat = 200
     public var width: CGFloat = 0
     public var paddingTop: CGFloat = 10
     public var cornerRadius: CGFloat = 25
     public var padding: CGFloat = 0
+
+    // Add this public initializer
+    public init(image: String, height: CGFloat = 200, width: CGFloat = 0, paddingTop: CGFloat = 10, cornerRadius: CGFloat = 25, padding: CGFloat = 0) {
+        self.image = image
+        self.height = height
+        self.width = width
+        self.paddingTop = paddingTop
+        self.cornerRadius = cornerRadius
+        self.padding = padding
+    }
+
     public var body: some View {
         Image(image)
             .resizable()
-            //.scaledToFit() // Ensures the image fills the specified frame
-            .frame(width: width, height: height) // Specify only the height; width will adjust automatically
-            .cornerRadius(cornerRadius) // Apply corner radius
-            .padding(.horizontal, padding) // Apply horizontal padding to the image container
+            .frame(width: width, height: height)
+            .cornerRadius(cornerRadius)
+            .padding(.horizontal, padding)
             .padding(.top, paddingTop)
-            .clipped() // Clips any overflowing content to fit within the corner radius
+            .clipped()
     }
-    
 }
